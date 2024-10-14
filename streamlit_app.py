9e5a00294504a5229ac776153ea09ea669687e1f
Yantong Cui <143852554+yanc0828@users.noreply.github.com>
2024-10-13 22:58:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -126,7 +126,8 @@ def task3():
                   width=20,
                   ).configure_facet(
                       spacing=0 # Adjust the spacing between columns (grouped bar spacing)
-                      )
+                      ).configure_concat(spacing=0  # Remove additional spacing between charts
+        )
       
       st.altair_chart(chart, use_container_width=True)
 
